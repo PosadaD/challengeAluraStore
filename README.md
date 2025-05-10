04a80fabfd9673a822c7931a986407afb2c613d9
DIEGO POSADA <dapiposada@gmail.com>
2025-05-09 18:20:53 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # challengeAluraStore
+
+## El propósito del análisis realizado.
+
+El propósito de este análisis es evaluar el rendimiento de varias tiendas online en función de diversos indicadores clave como la facturación, la calificación promedio, el número de productos más vendidos, y el costo de envío promedio. Se busca identificar qué tienda presenta el mejor desempeño global y qué tienda podría ser la candidata para cerrar, basándose en criterios como la rentabilidad, la satisfacción del cliente y la diversidad de productos populares.
+
+## La estructura del proyecto y organización de los archivos.
+/nombre-del-proyecto
+│
+├── /base-de-datos-challenge1-latam/    # carpeta de bases de datos
+│   ├── tienda_1.csv                    # base de datos en formato .csv
+│   ├── tienda_2.csv                    # base de datos en formato .csv
+│   ├── tienda_3.csv                    # base de datos en formato .csv
+│   ├── tienda_4.csv                    # base de datos en formato .csv
+│
+├── index.py                            # archivo de ejecucion del programa
+|
+├── /.venv/                             # carpeta del entorno virtual
+|
+├── .gitignore                          # Archivos y directorios a ignorar por Git
+|
+└── README.md                           # Descripción general del proyecto
+
+
+## Instrucciones para ejecutar.
+
+Para ejecutar el notebook y replicar el análisis, sigue estos pasos:
+
+1. Clona este repositorio en tu máquina local:
+   ```bash
+   git clone https://github.com/tu_usuario/tu_repositorio.git
+   cd tu_repositorio
+
+2. Asegúrate de tener instalado Python 3.x. Si no lo tienes, puedes instalarlo desde python.org.
+
+3. Crea un entorno virtual para evitar conflictos de dependencias:
+    python -m venv venv
+    source venv/bin/activate  # En Linux o macOS
+    venv\Scripts\activate     # En Windows
+
+4. Instala las dependencias necesarias usando pip:
+    pip install -r requirements.txt
+
+5. Run python file
+
---
+++ index.py
@@ -0,0 +1,74 @@
+import pandas
+
+tienda1_df = pandas.read_csv("base-de-datos-challenge1-latam/tienda_1.csv", header='infer');
+tienda2_df = pandas.read_csv("base-de-datos-challenge1-latam/tienda_2.csv", header='infer');
+tienda3_df = pandas.read_csv("base-de-datos-challenge1-latam/tienda_3.csv", header='infer');
+tienda4_df = pandas.read_csv("base-de-datos-challenge1-latam/tienda_4.csv", header='infer');
+
+tiendas = [tienda1_df, tienda2_df, tienda3_df, tienda4_df];
+
+#HEADERS
+#'Producto' 'Categoría del Producto' 'Precio' 'Costo de envío' 'Fecha de Compra' 'Vendedor' 'Lugar de Compra' 'Calificación' 'Método de pago' 'Cantidad de cuotas' 'lat' 'lon'
+
+facturacion = {};
+categoriasporTienda = {};
+calificacionPromedioTienda = {};
+masVendidos = {}
+costoEnivoPromedioTienda = {}
+
+for x in range(1, 5):
+    tienda = tiendas[x-1]
+    totalFacturacion = 0;
+
+    for y in range(0, len(tienda)):
+        
+        # 1. Análisis de facturación
+        #facturacion total de cada tienda 
+        precio = tienda.loc[y, "Precio"];
+        totalFacturacion += precio
+
+    facturacion[f"tienda_{x}"] = float(totalFacturacion);
+
+
+    # 2. Ventas por categoría
+    # categorias mas populares 
+    ventaCategoria = tienda["Categoría del Producto"].value_counts(); 
+    ventaCategoria = ventaCategoria.to_dict()
+    categoriasporTienda[f"tienda_{x}"] = ventaCategoria;
+
+            
+    # 3. Calificación promedio de la tienda
+    #Promedio de calificacion de los clientes
+    calificaciones = tienda["Calificación"];
+    calificaciones = calificaciones.to_list();
+    calificacionPromedioTienda[f"tienda_{x}"] = round(sum(calificaciones) / len(tienda), 2);
+
+
+    # 4. Productos más y menos vendidos
+    ventasProducto = tienda["Producto"].value_counts(); 
+    ventasProducto = ventasProducto.to_dict()
+    masVendidos[f"tienda_{x}"] = ventasProducto;
+
+
+    # 5. Envío promedio por tienda    
+    #costo pormedio de envio 
+    costoEnvio = tienda["Costo de envío"];
+    costoEnvio = costoEnvio.to_list();
+    costoEnivoPromedioTienda[f"tienda_{x}"] = round(sum(costoEnvio) / len(tienda), 2);
+
+
+print(facturacion);
+print(categoriasporTienda);
+print(calificacionPromedioTienda);
+print(masVendidos);
+print(costoEnivoPromedioTienda);
+
+
+
+     
+##conclusion: 
+# Tienda 4 tiene la facturación más baja, pero tiene una calificación promedio bastante competitiva (4.00), similar a la de Tienda 1 (3.98). Sin embargo, su facturación es significativamente más baja que las demás tiendas, lo que podría indicar que su rendimiento general no está a la par con las otras tiendas.
+
+# Tienda 1 tiene una facturación más alta, pero su calificación es la más baja (3.98), lo que podría sugerir problemas con la satisfacción del cliente.
+
+# En base a estos factores, la Tienda 4 parece ser la candidata más probable para cerrar, dado que tiene una combinación de menor facturación y una cantidad de productos populares no muy destacada. Sin embargo, si la satisfacción del cliente es más crítica, la Tienda 1 también podría ser considerada debido a su baja calificación.
